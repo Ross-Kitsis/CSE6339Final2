819100a01e0d40c7d15bd23f75ca30a3cb337c57
Ross-Laptop <ross.kitsis@gmail.com>
2015-04-22 20:42:01 -0400

    Continued Bitstring implementation, starting CNG

--- NgramOps.java
+++ NgramOps.java
@@ -28,5 +28,35 @@ public class NgramOps
 		
 		return toReturn;
 	}
+	/**
+	 * Extracts byte n-grams from the passed array, converts them to bit string and 
+	 * adds them to a list
+	 * 
+	 * @param bytes An array from where to extract n-grams
+	 * @param size The size of the n-grams to extract
+	 * @return A list containing the n-grams extracted
+	 */
+	public List<String> getBitStringNgramsFromByteArray(byte[] bytes, int size)
+	{
+		List<String> toReturn = new ArrayList<String>();
+		StringBuilder sb = new StringBuilder();
+		
+		for(int start = 0; start < bytes.length - size; start++)
+		{
+			for(int i = 0; i < size; i++)
+			{
+				sb.append(String.format("%8s", Integer.toBinaryString(bytes[i] & 0xFF)).replace(' ', '0'));
+			}
+			toReturn.add(sb.toString());
+			sb.setLength(0);
+		}
+		
+		return toReturn;
+	}
+	public String findProbable(Profile p, List<Profile> q)
+	{
+		String auth = "";
+		return auth;
+	}
 	
 }
--- Profile.java
+++ Profile.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Profile
 {
 	private String idenifier;
-	private LinkedArrayHashMap<Byte[],Integer> ngrams = new LinkedArrayHashMap<Byte[],Integer>();
+	private Map<String,Integer> ngrams = new LinkedHashMap<String,Integer>();
 	private int ngramSize;
 	
 	
@@ -17,18 +17,66 @@ public class Profile
 	 * Builds a profile by traversing the list of bytes and filling a map with n-grams and the number of occurrences
 	 * @param bytes
 	 */
-	public void buildProfile(List<Byte[]> bytes)
+	public void addToProfile(List<String> allNgrams)
 	{
-		for(int i = 0; i < bytes.size(); i++)
+		String toAdd;
+		int n;
+		for(int i = 0; i < ngrams.size(); i++)
 		{
-			Byte[] toAdd = bytes.get(i);
-			if(!ngrams.containsArrayKey(toAdd))
+			toAdd = allNgrams.get(i);
+			if(ngrams.containsKey(toAdd))
 			{
-				ngrams.put(toAdd, 0);
+				n = ngrams.get(i);
+				ngrams.put(toAdd, n+1);
 			}else
 			{
-				
+				ngrams.put(toAdd, 1);
 			}
 		}
+	}
+	public String getIdenifier() {
+		return idenifier;
+	}
+	public Map<String, Integer> getNgrams() {
+		return ngrams;
+	}
+	public int getNgramSize() {
+		return ngramSize;
+	}
+	/**
+	 * Sets a profile to the Lth most popular ngrams
+	 * @param length The number of n-grams to include
+	 */
+	public void setProfileLength(int length)
+	{
+		if(ngrams.size() < length)
+		{
+			return;
+		}
+		
+		Map<String,Integer> temp = new LinkedHashMap<String,Integer>();
+		
+		int max = Integer.MIN_VALUE;
+		String key = null;
+		int v;
+		
+		for(int i = 0; i < length; i++)
+		{
+			Set<String> keys = ngrams.keySet();
+			for(String s:keys)
+			{
+				v = ngrams.get(s);
+				if(v > max)
+				{
+					max = v;
+					key = s;
+				}
+			}
+			temp.put(key, max);
+			ngrams.remove(key);
+			max = Integer.MAX_VALUE;
+		}
+		
+		ngrams = temp;
 	}
 }
